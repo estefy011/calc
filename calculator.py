c62462d9a457868585cce806d55f8ac2455922cc
estefy011 <90658818+estefy011@users.noreply.github.com>
2024-10-21 21:22:18 -0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
